678b46234d074c83f735167946c920da5314076c
Allyson Vieira <allyson.vieira@targetwork.com.br>
2025-06-22 22:33:20 -0300

    update port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createNestServer, expressServer } from './server';
 async function bootstrap() {
   const app = await createNestServer(expressServer);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 
 bootstrap();
